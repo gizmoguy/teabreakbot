abbc9aea6d425d738a4d2f36e4666dea8d39aba7
Nathan Overall <git@shweppsie.com>
2012-09-22 19:12:09 +1200

    Move config parser into a separate file

---
+++ teabreakbotconfig.py
@@ -0,0 +1,24 @@
+import ConfigParser
+import os
+
+def read_config():
+    filenames = ['/etc/teabreakbot.cfg',
+            os.path.expanduser('~/.teabreakbot.cfg'),
+            'teabreakbot.cfg']
+
+    config=ConfigParser.RawConfigParser()
+
+    config.add_section("irc")
+    config.set("irc","host","localhost")
+    config.set("irc","port","6667")
+
+    config.add_section("twitter")
+    config.set("twitter","enabled","false")
+    config.set("twitter","consumer_key","")
+    config.set("twitter","consumer_secret","")
+    config.set("twitter","access_key","")
+    config.set("twitter","access_secret","")
+
+    config.read(filenames)
+    return config
+
